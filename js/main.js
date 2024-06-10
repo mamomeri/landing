e1611ab01d1ad0d72d4888d4aa83535bf2d2498e
mamomeri <mamomeri@espol.edu.ec>
2024-06-10 10:03:58 -0500

    reformulacion

--- js/main.js
+++ js/main.js
@@ -7,3 +7,31 @@ let loaded = (eventLoaded) => {
 }
 
 window.addEventListener("DOMContentLoaded", loaded);
+
+let completarFormulario = ()=>{
+    const formulario = document.getElementById('formulario');
+    formulario.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const nombre = document.getElementById('nombre').value;
+    const email = document.getElementById('email').value;
+    const mensaje = document.getElementById('mensaje').value;
+    const datos = {
+    nombre: nombre,
+    email: email,
+    mensaje: mensaje
+    };
+    fetch('https://dawmproyecto-default-rtdb.firebaseio.com/coleccion.json', {
+    method: 'POST',
+    body: JSON.stringify(datos),
+    headers: {
+    'Content-Type': 'application/json'
+    }
+    })
+    .then(respuesta => respuesta.json())
+    .then(datos => {
+    console.log(datos); 
+    servidor
+    })
+    .catch(error => console.error(error));
+    });
+} 
